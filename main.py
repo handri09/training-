0460981ab3c6de124931798df2fd55f1750f9773
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2021-01-24 15:37:00 +0300

    num 26: PEP Guidelines

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+''' Section 3: Advanced Python Concepts '''
 
-# Greetings
+''' PEP guidelines
+	Best practice for code format '''
+
+# Function
 def print_hi(name):
     print(f'Hi, {name}!')
 
+def my_function(one, two,
+				three, four,
+				five, six):
+	print("Hello World")
+
+my_list = [1, 2,
+		   3, 4,
+		   5, 6]
+
+prin("Hello ", "Hi")
+
+x = 3 * 53 + 7*2
+
+func_one()
+func_two()
+func_three()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Haja')
